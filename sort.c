6efbebf8bd230047192cf1f0222c63b2d01b1bd9
Nakayama Haruto <e245757@ie.u-ryukyu.ac.jp>
2025-01-21 22:34:11 +0900

    1st commit

--- sort.c
+++ sort.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 void sortAscending(int numList[], int size) {
-    for (int i = 0; i < size - 1;i ++ ) {
-        for (int j = size - 1; j > i; j --) {
-            if (numList[j - 1] < numList[j]) {
-                int swap = numList[j];
-                numList[j] = numList[j - 1];
-                numList[j - 1] = swap;
+    for (int i = 0; i < size;i ++ ) {
+        for (int j = i + 1; j < size; j ++) {
+            if (numList[j] < numList[i]) {
+                int temp = numList[i];
+                numList[i] = numList[j];
+                numList[j] = temp;
             }
         }
     }
@@ -23,6 +23,8 @@ void sortAscending(int numList[], int size) {
 int main(void){
     const int SIZE = 5; //定数として配列のサイズを宣言                          
     int scores[SIZE] = {0, 60, 70, 100, 90};
+    // const int SIZE = 6; //定数として配列のサイズを宣言
+    // int scores[SIZE] = {100, 60, 70, 100, 90, 80};
     sortAscending(scores, SIZE);
     return 0;
 }
